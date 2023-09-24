6a0e7069a50860ab292d5d67460cbb85276ab2c9
Sheldon Trotman <3892892+sheldot@users.noreply.github.com>
2023-09-24 14:28:28 -0400

    merge conflicts

--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -6,7 +6,7 @@ import Header from "./Header";
 import RunModelScreen from "./RunModelScreen";
 import Footer from "./Footer";
 import ConfirmTransactionScreen from "./ConfirmTransactionScreen";
-import Axios from 'axios';
+import Axios from "axios";
 
 const MainScreen = ({
   account,
@@ -22,86 +22,102 @@ const MainScreen = ({
     React.useState(null);
   const onClick = (newState) => {
     setCurrentPageIndex(newState);
-
   };
-  const [searchCount, setSearchCount] = React.useState(0);
+  const [searchCount, setSearchCount] =
+    React.useState(0);
 
-
-
-  const sdasd = async ({onClickHandler}) => {
-
-    const toolsDataString = JSON.stringify({ args: [] });
+  const sdasd = async ({ onClickHandler }) => {
+    const toolsDataString = JSON.stringify({
+      args: [],
+    });
     console.log(toolsDataString);
     try {
       const response = await Axios.post(
         "http://198.166.138.214:5381/create_plan/", // Add the protocol (http:// or https://) here
-        { data: toolsDataString }
+        { data: toolsDataString },
       );
 
       // console.log(response.data);
       console.log(response.data);
       const pulledData = {
-        "data": [
+        data: [
           {
-            "name": "tool.get_best_apy",
-            "description": "\"Get the best apy between 2 dictionaries",
-            "tags": ["defi", "tool"],
-            "schema": {
-              "input": { "data1": "<class 'dict'>", "data2": "<class 'dict'>" },
-              "output": null,
-              "default": { "data1": null, "data2": null }
-            },
-            "price": 99
-          },
-          {
-            "name": "tool.defillama.rocketpool",
-            "description": "\n        Connects to the Defillama API and allows the user to select which chain, project, symbol or pool they want. \n        :param params: A dictionary with optional filters (chain (first letter uppercase), project, symbol, pool).\n        :return: Filtered list of pool data.\n\n        Example input: \n        # Fetch data for a specific chain and project\n        params = {\n            \"chain\": \"Ethereum\",\n            \"project\": \"lido\",\n        }\n\n        here is an input:\n        rocket_pool_instance = RocketPool()\n        result=rocket_pool_instance.call(project=\"rocket-pool\", symbol=\"RETH\")\n        here is an example of the output that corresponds with the above input:\n        [{'apy': 3.21066, 'market': 'rocket-pool', 'asset': 'RETH', 'chain': 'Ethereum', 'timestamp': 1695494506.412746}]\n    ",
-            "tags": ["defi", "tool"],
-            "schema": {
-              "input": {
-                "chain": "<class 'str'>",
-                "project": "<class 'str'>",
-                "symbol": "<class 'str'>"
+            name: "tool.get_best_apy",
+            description:
+              '"Get the best apy between 2 dictionaries',
+            tags: ["defi", "tool"],
+            schema: {
+              input: {
+                data1: "<class 'dict'>",
+                data2: "<class 'dict'>",
               },
-              "output": null,
-              "default": { "chain": null, "project": "rocket-pool", "symbol": null }
+              output: null,
+              default: {
+                data1: null,
+                data2: null,
+              },
             },
-            "price": 45
+            price: 99,
           },
           {
-            "name": "tool.inch.gasprice",
-            "description": "\n    Gets the token prices from the 1inch API.\n    ",
-            "tags": ["defi", "tool"],
-            "schema": { "input": {}, "output": null, "default": {} },
-            "price": 27
-          }
-        ]
-      }
-      
-      const parsedData = searchCount === 0 ?pulledData.data : JSON.parse(response.data).data
-      
+            name: "tool.defillama.rocketpool",
+            description:
+              "\n        Connects to the Defillama API and allows the user to select which chain, project, symbol or pool they want. \n        :param params: A dictionary with optional filters (chain (first letter uppercase), project, symbol, pool).\n        :return: Filtered list of pool data.\n\n        Example input: \n        # Fetch data for a specific chain and project\n        params = {\n            \"chain\": \"Ethereum\",\n            \"project\": \"lido\",\n        }\n\n        here is an input:\n        rocket_pool_instance = RocketPool()\n        result=rocket_pool_instance.call(project=\"rocket-pool\", symbol=\"RETH\")\n        here is an example of the output that corresponds with the above input:\n        [{'apy': 3.21066, 'market': 'rocket-pool', 'asset': 'RETH', 'chain': 'Ethereum', 'timestamp': 1695494506.412746}]\n    ",
+            tags: ["defi", "tool"],
+            schema: {
+              input: {
+                chain: "<class 'str'>",
+                project: "<class 'str'>",
+                symbol: "<class 'str'>",
+              },
+              output: null,
+              default: {
+                chain: null,
+                project: "rocket-pool",
+                symbol: null,
+              },
+            },
+            price: 45,
+          },
+          {
+            name: "tool.inch.gasprice",
+            description:
+              "\n    Gets the token prices from the 1inch API.\n    ",
+            tags: ["defi", "tool"],
+            schema: {
+              input: {},
+              output: null,
+              default: {},
+            },
+            price: 27,
+          },
+        ],
+      };
+
+      const parsedData =
+        searchCount === 0
+          ? pulledData.data
+          : JSON.parse(response.data).data;
+
       //load JSON string
       // const parsedData = JSON.parse(dataShown).data;
-      
-      console.log(parsedData );
-    // Set the parsed data in the state
-        // setJsonData(parsedData);
-        
 
-      
-      
+      console.log(parsedData);
+      // Set the parsed data in the state
+      // setJsonData(parsedData);
+
       // console.log(extractedData);
-      setSearchCount(1)
-      setReturnPayload(parsedData)
+      setSearchCount(1);
+      setReturnPayload(parsedData);
       onClickHandler(2);
-
     } catch (error) {
       // Handle any errors from the POST request
-      console.error("Error making POST request:", error);
+      console.error(
+        "Error making POST request:",
+        error,
+      );
     }
-
-
-  }
+  };
 
   return (
     <div>
@@ -116,7 +132,10 @@ const MainScreen = ({
         isMetamaskAuth={isMetamaskAuth}
       />
       {currentPageIndex == 0 ? (
-        <GridMarket onClickHandler={onClick} sdasd={sdasd} />
+        <GridMarket
+          onClickHandler={onClick}
+          sdasd={sdasd}
+        />
       ) : currentPageIndex == 1 ? (
         <CustomToolScreen
           onClickHandler={onClick}
@@ -127,13 +146,10 @@ const MainScreen = ({
           loggedInProvider={loggedInProvider}
         />
       ) : (
-<<<<<<< HEAD
-        <ConfirmTransactionScreen returnPayload={returnPayload} onClickHandler={onClick} />
-=======
         <ConfirmTransactionScreen
+          returnPayload={returnPayload}
           onClickHandler={onClick}
         />
->>>>>>> f12cfdf (updates for completeness)
       )}
       <Footer />
     </div>
